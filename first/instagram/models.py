e336a145788927d09f1fe58458d95036b414730b
dongkyeomjang <kyeom@jangdong-gyeom-ui-MacBookPro.local>
2023-11-01 11:06:44 +0900

    코드 오류 수정

--- first/instagram/models.py
+++ first/instagram/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 class Projfile(models.Model):
-    user = models.OnetoOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Profile은 settings.AUTH_USER_MODEL과 1대1 관계. 즉 한 명의 유저는 하나의 프로필만 가질 수 있음
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Profile은 settings.AUTH_USER_MODEL과 1대1 관계. 즉 한 명의 유저는 하나의 프로필만 가질 수 있음
     blog_url = models.URLField(blank=True)
 
 class Post(models.Model): 
@@ -12,7 +12,7 @@ class Post(models.Model):
     desc = models.TextField(blank=True) #blank=True는 빈칸을 허용한다는 뜻
     image= models.ImageField(blank=True) #blank=True는 빈칸을 허용한다는 뜻
     comment_count= models.PositiveIntegerField(default=0) #PositiveIntegerField는 양수만 허용한다는 뜻
-    tag_set = models.ManyToMany('Tag', blank=True) #ManyToMany는 다대다 관계를 의미함. Tag는 아래에 정의되어 있음
+    tag_set = models.ManyToManyField('Tag', blank=True) #ManyToMany는 다대다 관계를 의미함. Tag는 아래에 정의되어 있음
     is_publish = models.BooleanField(default=False) #BooleanField는 True/False만 허용한다는 뜻
     created_at = models.DateTimeField(auto_now_add=True) #auto_now_add=True는 생성시간을 자동으로 넣어줌
     updated_at = models.DateTimeField(auto_now=True) #auto_now=True는 수정시간을 d자동으로 넣어줌
